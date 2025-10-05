1ee027f55c83dd8e8b922e2a0ad1bf8810d4afc3
admin <id0rin01@yandex.ru>
2025-10-05 23:52:01 +0300

    the beginning is the creation of functionality, the creation of a keyboard, you need to work out the logic of getting deadlines

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import TOKEN
-import sqlite3
 import telebot
+from telebot import types
 from DB import createDatabase
 from DB import funcForTasks as FFT
 
@@ -14,7 +14,12 @@ createDatabase()
 
 
 
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+item1 = types.KeyboardButton("записать новый дедлайн")
+item2 = types.KeyboardButton("посмотреть мои дедлайны")
+item3 = types.KeyboardButton("удалить дедлайн")
 
+markup.add(item1, item2, item3)
 
 
 
@@ -27,7 +32,18 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.send_message(message.chat.id, 'салам погнали', parse_mode='html', reply_markup=markup)
+
+
+
+@bot.message_handler(content_types=['text'])
+def working(message):
+    if message.text == "записать новый дедлайн":
+
+
+
+
+
+
+
+bot.infinity_polling()
