1a1bee398db4b949042d15f51a8a32701bbe57f9
m1pr0 <106233051+m1pr0@users.noreply.github.com>
2025-11-12 00:58:52 +0300

    create: "userRegistry" update: processing a non-existent command

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 
 from DB import createDatabase
 from DB import funcForTasks as FFT
-from config import TOKEN
+from config import TOKEN, userRegistry
 from suportFuncs import before_create, show_tasks, before_update, randomStic, actual_tasks
 
 createDatabase()
@@ -34,38 +34,43 @@ def working(message):
     username = message.from_user.username
     chat_id = message.chat.id
 
-    if message.text == "📝 Новая задача":
-        msg = bot.send_message(message.chat.id, "введите задание и дедлайн в следуюхем формате: задание|дедлайн")
-        bot.register_next_step_handler(msg, before_create, username)
-        randomStic(bot, chat_id)
-        # bot.send_message(message.chat.id, f"задача создана")
+    if username in userRegistry:
 
-    elif message.text == "✏️ Обновить":
-        msg = bot.send_message(message.chat.id,
-                               "введите обновленное задание и дедлайн в следуюхем формате: номер задачи|задание|дедлайн")
-        bot.register_next_step_handler(msg, before_update, username)
-        randomStic(bot, chat_id)
-        # bot.send_message(message.chat.id, f"задача обновлена")
+        if message.text == "📝 Новая задача":
+            msg = bot.send_message(message.chat.id, "введите задание и дедлайн в следуюхем формате: задание|дедлайн")
+            bot.register_next_step_handler(msg, before_create, username)
+            randomStic(bot, chat_id)
+            # bot.send_message(message.chat.id, f"задача создана")
 
-
-    elif message.text == "👀 Мои задачи":
-        tasks = [x["id"] for x in actual_tasks(username)]
-        msg = bot.send_message(message.chat.id,f"введите номер задачи, если хотите посмотреть все задачи, введите: 'все'\n\nактуальные задачи: {tasks}")
-        bot.register_next_step_handler(msg, show_tasks, username, chat_id, bot)
-        randomStic(bot, chat_id)
+        elif message.text == "✏️ Обновить":
+            msg = bot.send_message(message.chat.id,
+                                   "введите обновленное задание и дедлайн в следуюхем формате: номер задачи|задание|дедлайн")
+            bot.register_next_step_handler(msg, before_update, username)
+            randomStic(bot, chat_id)
+            # bot.send_message(message.chat.id, f"задача обновлена")
 
 
-    elif message.text == "✅ Завершить":
-        msg = bot.send_message(message.chat.id, "введите номер задачи, которую нужно завершить")
-        bot.register_next_step_handler(msg, FFT.CompletedTask, username)
-        randomStic(bot, chat_id)
+        elif message.text == "👀 Мои задачи":
+            tasks = [x["id"] for x in actual_tasks(username)]
+            msg = bot.send_message(message.chat.id,f"введите номер задачи, если хотите посмотреть все задачи, введите: 'все'\n\nактуальные задачи: {tasks}")
+            bot.register_next_step_handler(msg, show_tasks, username, chat_id, bot)
+            randomStic(bot, chat_id)
 
-    elif message.text == "📋 Завершенные":
-        comTasks = FFT.watchCompleted(username)
-        for task in comTasks:
-            task_info = f"ID: {task['id']}\nТекст: {task['text']}\nДедлайн: {task['deadline']}"
-            bot.send_message(message.chat.id, task_info)
-        randomStic(bot, chat_id)
+
+        elif message.text == "✅ Завершить":
+            msg = bot.send_message(message.chat.id, "введите номер задачи, которую нужно завершить")
+            bot.register_next_step_handler(msg, FFT.CompletedTask, username)
+            randomStic(bot, chat_id)
+
+        elif message.text == "📋 Завершенные":
+            comTasks = FFT.watchCompleted(username)
+            for task in comTasks:
+                task_info = f"ID: {task['id']}\nТекст: {task['text']}\nДедлайн: {task['deadline']}"
+                bot.send_message(message.chat.id, task_info)
+            randomStic(bot, chat_id)
+
+        else:
+            bot.send_message(message.chat.id, "такой функции нет, воспользуйтесь предложенной ")
 
 
 bot.infinity_polling()
